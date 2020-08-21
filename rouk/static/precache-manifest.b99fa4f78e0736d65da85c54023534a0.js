4a0f8642ca87ed8c34676ed9ef940a178db08764
Andres Sierra <cepfelo@gmail.com>
2020-08-20 21:26:05 -0500

    added rouk.sh and rouk.service, and documentation on how to install the app was added to README

---
+++ rouk/static/precache-manifest.b99fa4f78e0736d65da85c54023534a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "303e69f888f615a480c7",
+    "url": "/static/css/app.f06ed5c8.css"
+  },
+  {
+    "revision": "1544b2533c36a60ea6d5bfc398111a64",
+    "url": "/static/index.html"
+  },
+  {
+    "revision": "303e69f888f615a480c7",
+    "url": "/static/js/app.05dceac6.js"
+  },
+  {
+    "revision": "cd27a02f9a6070eea4df",
+    "url": "/static/js/chunk-vendors.0cf8892a.js"
+  },
+  {
+    "revision": "a635afe5aea008f3e94771bdb2ed07bd",
+    "url": "/static/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/static/robots.txt"
+  }
+]);
